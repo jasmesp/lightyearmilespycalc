724b2987d5b4213f145142bcd9ef4fd0d47f18c9
Jas <jasmes@gmail.com>
2021-10-05 23:46:49 -0400

    try2

--- lightyearcalc.py
+++ lightyearcalc.py
@@ -8,11 +8,18 @@ def ly_to_miles(arg):
     z = x * y
     return arg * z
 
+def ly_to_km(arg):
+    x = 9.46
+    y = (10**12)
+    z = x*y
+    return arg*z
 
 def main():
     light_years = get_in("light years:")
     miles = ly_to_miles(light_years)
+    km = ly_to_km(light_years)
     print(str(miles) + " miles")
+    print(str(km) + "kilometers")
 
 
 main()
