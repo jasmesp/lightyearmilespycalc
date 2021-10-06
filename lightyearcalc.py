5b6a212d1bed1509c607e98043a4553dd0a5823b
Jas <jasmes@gmail.com>
2021-10-05 23:43:41 -0400

    try2

---
+++ lightyearcalc.py
@@ -0,0 +1,18 @@
+def get_in(arg):
+    return float(input(arg))
+
+
+def ly_to_miles(arg):
+    x = 5.88
+    y = (10 ** 12)
+    z = x * y
+    return arg * z
+
+
+def main():
+    light_years = get_in("light years:")
+    miles = ly_to_miles(light_years)
+    print(str(miles) + " miles")
+
+
+main()
